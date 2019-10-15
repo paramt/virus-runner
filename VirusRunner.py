4c6e27ab574422b8e7af92e0a0fd71e15ee518cb
Param Thakkar <pht0104@gmail.com>
2019-10-15 17:32:59 -0400

    Simplify title screen code

--- VirusRunner.py
+++ VirusRunner.py
@@ -93,9 +93,9 @@ class VirusRunner(arcade.Window):
         arcade.draw_text("Press   any   button   to   restart", 425, 350, arcade.csscolor.WHITE, 30, font_name="assets/fonts/ARCADECLASSIC.ttf")
 
     def draw_title_screen(self):
-        self.background = arcade.load_texture("assets/sprites/background.png")
+        background = arcade.load_texture("assets/sprites/background.png")
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, background)
 
     def on_draw(self):
         """ Render the screen """
